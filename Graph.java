81ce9186a7b4ed2859f49d586249b5f47eff371a
Lester-Rodrigo <alrodrinew@gmail.com>
2026-05-19 18:50:17 -0600

    Floyd creado

--- Graph.java
+++ Graph.java
@@ -3,6 +3,7 @@ import java.util.Map;
 
 public class Graph {
     public int [][] matrix;
+    public int[][] floydMatrix;
     Map<String, Integer> vertexs = new HashMap<>();
 
     public Graph(){   
@@ -27,23 +28,106 @@ public class Graph {
             matrix[aux.length][aux.length] = -1;
             }
         }
+        floyd();
     }
 
     public void addEdge(int vertex1, int vertex2, int weight){
+        if (vertex1 >= matrix.length || vertex2 >= matrix.length) {
+            System.out.println("Vertice inválido");
+            return;
+        }
+
         if (matrix[vertex1][vertex2] == -1) {
             matrix[vertex1][vertex2] = weight;
+            floyd();
         }
         else{
             System.out.println("Arista ya existe");
         }
     }
 
+    public void removeEdge(int vertex1, int vertex2){
+
+        if (vertex1 >= matrix.length || vertex2 >= matrix.length) {
+            System.out.println("Vertice inválido");
+            return;
+        }
+        if (matrix[vertex1][vertex2] != -1) {
+            matrix[vertex1][vertex2] = -1;
+            floyd();
+            System.out.println("Arista eliminada");
+        }
+        else{
+            System.out.println("No existe una arista entre esos vértices");
+        }
+    }
+
+    public int[][] floyd(){
+        int vertexs = matrix.length;
+        int[][] edge_dist = new int[vertexs][vertexs];
+
+        for (int i = 0; i < vertexs; i++) {
+            for (int j = 0; j < vertexs; j++) {
+                edge_dist[i][j] = matrix[i][j];
+                if (i == j) {
+                    edge_dist[i][j] = 0;
+                }
+                if (edge_dist[i][j] == -1 && i != j) {
+                    edge_dist[i][j] = 999999;
+                }
+            }
+        }
+
+        for (int i = 0; i < vertexs; i++) {
+            for (int j = 0; j < vertexs; j++) {
+                for (int k = 0; k < vertexs; k++) {
+                    if (edge_dist[k][i] + edge_dist[i][j] < edge_dist[k][j]) {
+                        edge_dist[k][j] = edge_dist[k][i] + edge_dist[i][j];
+                    }
+                }
+            }
+        }
+        floydMatrix = edge_dist;
+        return edge_dist;
+    }
+
     public void printGraph(){
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j] + " ");
+        if (matrix != null) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix.length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+        else {
+            System.out.println("El grafo está vacío");
+        }
+    }
+
+    public void printVertexs(){
+        if (matrix != null) {
+            for (String vertex : vertexs.keySet()) {
+            System.out.println(vertex);
+            }
+        }
+        else {
+            System.out.println("El grafo está vacío");
+        }
+    }
+
+      public void printFloyd(int[][] dist){
+        for (int i = 0; i < dist.length; i++) {
+            for (int j = 0; j < dist.length; j++) {
+                if (dist[i][j] == 999999) {
+                    System.out.print("INF ");
+                }
+                else{
+                    System.out.print(dist[i][j] + " ");
+                }
             }
             System.out.println();
         }
     }
+    
 }
--- Main.java
+++ Main.java
@@ -5,9 +5,11 @@ public class Main {
         graph.addVertex("A");
         graph.addVertex("B");
         graph.addVertex("C");
+        graph.addEdge(0,1,5);
+        graph.addEdge(1,2,3);
+        graph.addEdge(0,2,20);
         graph.printGraph();
-        graph.addEdge(0, 1, 5);
-        graph.addEdge(1, 2, 10);
-        graph.printGraph();
+        int[][] result = graph.floyd();
+        graph.printFloyd(result);
     }
 }
